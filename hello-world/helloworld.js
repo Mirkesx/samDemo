58fd4183796cdddf86f6cf6f7e083de574543809
Marco Cavalli <cmarcos46@gmail.com>
2020-12-31 16:26:12 +0100

    Update helloworld.js

--- hello-world/helloworld.js
+++ hello-world/helloworld.js
@@ -20,7 +20,7 @@ exports.helloWorldHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world!!',
+                message: 'Hello World Production',
                 // location: ret.data.trim()
             })
         }
